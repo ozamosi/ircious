9fdf66672e4f40e09df9c757a8229f0e932de37c
Robin Sonefors <ozamosi@flukkost.nu>
2007-11-04 21:12:56 +0100

    Fix the part I forgot about...

--- django-projects/ircious/media/favscript.js
+++ django-projects/ircious/media/favscript.js
@@ -1,6 +1,6 @@
 function setFavourite(object)
 {
-    var id = object.id.substr(10);
+    var id = object.getAttribute("id").substr(3, object.id.length-1);
     var request = new XMLHttpRequest();
     request.open('POST', '/'+id+'/favourite/', true);
     request.onreadystatechange = function()
